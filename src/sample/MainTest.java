9844535ebf7d7f8942954ad5489f4e870daab9e6
ssukigara <sukisukigaragara@gmail.com>
2014-04-08 18:17:52 +0900

    Moved libs from Eclipse plugin to lib dir

--- src/sample/MainTest.java
+++ src/sample/MainTest.java
@@ -12,20 +12,7 @@ public class MainTest {
         Main.main(args);
 
         boolean expected = true;
-        boolean expected2 = false;
-        long lExpected = 0;
-        long lActual = 0;
-
         assertTrue(expected);
-        assertFalse(expected2);
-        assertNull(null);
-        assertEquals(lExpected, lActual);
-
-        String s0 = "abc";
-        String s1 = s0;
-        // ***** == でのテスト (true) *****
-        assert s0 == s1;
-        assertSame(s0, s1);
 
     }
 
